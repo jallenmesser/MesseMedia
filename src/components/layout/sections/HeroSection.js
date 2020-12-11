ad56a634bf2954cb874c2590c1906622e8fc91d2
jallenmesser <jallenmessersmith@gmail.com>
2020-12-11 12:53:18 -0500

    through lesson 10

--- src/components/layout/sections/HeroSection.js
+++ src/components/layout/sections/HeroSection.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import MockupAnimation from "../../animations/MockupAnimaton"
 import PurchaseButton from "../../buttons/PurchaseButton"
 import { themes } from "../../styles/ColorStyles"
 import { H1, MediumText } from "../../styles/TextStyles"
@@ -9,13 +10,14 @@ function HeroSection() {
     <Wrapper>
       <ContentWrapper>
         <TextWrapper>
-          <Title>Digital, Design, Development</Title>
+          <Title>Dev, Digital, Design</Title>
           <Description>We can make your life a breeze!</Description>
           <PurchaseButton
             title="Start Learning"
             subtitle="100+ hours of video"
           />
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -25,11 +27,14 @@ export default HeroSection
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 
 const TextWrapper = styled.div`
